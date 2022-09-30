b2e7769c9419218b526c9578277a51a622e76945
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-09-30 10:57:55 -0400

    feat: support ejecting the tool
    The goal of these changes is to make it more approchable to forget about the ci-update tool and manage the ci configs yourself.
    
    BREAKING CHANGE:
     - ember-try option now _only_ supports a value of `true` -- It willl always read from the ember-try.js config.
     - only pnpm is supported
    
    Tested here: https://github.com/NullVoxPopuli/ember-resources/pull/633

--- src/providers/github/templates/v2-addon/index.js
+++ src/providers/github/templates/v2-addon/index.js
@@ -1,25 +1,47 @@
-import assert from 'assert';
-import path from 'path';
-import fse from 'fs-extra';
-import { stripIndent } from 'common-tags';
+import assert from 'node:assert';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+import fse from 'fs-extra';
+import klaw from 'klaw';
 import yaml from 'js-yaml';
+import ejs from 'ejs';
 
 import { verifyConfig } from './config.js';
 import { getEmberTryNames } from './ember-try.js';
 
 const targetFile = '.github/workflows/ci.yml';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const templateFiles = path.join(__dirname, 'files');
+
+const stringify = (x) => `"${x}"`;
+const toYamlArray = (array) => `[${array.map(stringify).join(', ')}]`;
+const indent = (num) => {
+  let result = '';
+
+  for (let i = 0; i < num; i++) {
+    result += '  ';
+  }
+
+  return result;
+};
+const toMultiLineArray = (array, indentCount) => {
+  return array
+    .map((item) => `- ${item}`)
+    .map((line) => indent(indentCount) + line)
+    .join('\n');
+};
+const eachLine = (str, callback) => {
+  return str.split('\n').map(callback).join('\n');
+}
+
 /**
  * @param {import('types').GitHubConfig} config
  * @param {import('types').Options} options
  */
 export default async function v2Addon(config, options) {
-  let _config = verifyConfig(config);
-
-  let ci = await buildCi(_config, options);
-
-  await write(ci, options);
+  await buildCi(verifyConfig(config), options);
 }
 
 /**
@@ -36,91 +58,16 @@ async function write(ci, options) {
 }
 
 /**
- * TODO: determine what version "latest" is
- */
-const actions = {
-  pnpm: 'pnpm/action-setup@v2.2.2',
-  cache: 'actions/cache@v3',
-  checkout: 'actions/checkout@v3',
-  downloadArtifact: 'actions/download-artifact@v3',
-  uploadArtifact: 'actions/upload-artifact@v3',
-  volta: 'volta-cli/action@v1',
-  commitlint: 'wagoid/commitlint-github-action@v5.0.2',
-};
-
-let init = [{ uses: actions.checkout }, { uses: actions.volta }];
-
-let depCache = [
-  {
-    name: 'Cache pnpm modules',
-    uses: actions.cache,
-    with: {
-      path: '~/.pnpm-store',
-      key: `\${{ runner.os }}-\${{ hashFiles('**/pnpm-lock.yaml') }}`,
-      'restore-keys': '${{ runner.os }}-',
-    },
-  },
-];
-
-let pnpm = [
-  {
-    uses: actions.pnpm,
-    with: {
-      /**
-       * TODO: detect pnpm version based on package.json
-       *       information
-       */
-      version: '7.1.2',
-    },
-  },
-];
-
-let getDist = [
-  {
-    name: 'Download built package from cache',
-    uses: actions.downloadArtifact,
-    with: {
-      name: 'dist',
-      path: '${{ env.dist }}',
-    },
-  },
-];
-
-/**
  * @typedef {import('types').GitHubV2AddonConfig} Config
  *
  * @param {import('types').GitHubV2AddonConfig} config;
  * @param {import('types').Options} options
  */
 async function buildCi(config, options) {
-  let eslint = {};
-  let typescript = {};
-  let tryScenarios = {};
-  let publish = {};
-  let extras = {};
-
-  let {
-    defaultBranch = 'main',
-    // packageManager = 'pnpm',
-    addon,
-    testApp,
-    build,
-    support,
-    release,
-    extra,
-  } = config;
-
-  let system = {
-    'timeout-minutes': 5,
-    'runs-on': 'ubuntu-latest',
-  };
+  let { defaultBranch = 'main', addon, support, release, extra } = config;
 
   let libraryName = await verifyAddon(config, options);
   let testAppName = await verifyTestApp(config, options, libraryName);
-
-  let onlyPR = {
-    if: `github.ref != 'refs/heads/${defaultBranch}'`,
-  };
 
   let env = {
     CI: true,
@@ -130,85 +77,12 @@ async function buildCi(config, options) {
     dist: path.join(addon, 'dist'),
   };
 
-  let install = [{ name: 'Install Dependencies', run: 'pnpm install' }];
-  let installNoLockfile = [
-    { name: 'Install Dependencies (without lockfile)', run: 'rm pnpm-lock.yaml && pnpm install' },
-  ];
-  let installNoFrozen = [
-    {
-      name: 'Install Dependencies (without frozen lockfile)',
-      run: 'pnpm install --no-frozen-lockfile',
-    },
-  ];
-
   if (config.lint?.eslint) {
     await verifyEslint(config, options);
-
-    eslint = {
-      eslint: {
-        name: 'ESLint',
-        needs: ['install_dependencies'],
-        ...system,
-        strategy: {
-          'fail-fast': true,
-          matrix: {
-            path: [...config.lint.eslint],
-          },
-        },
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...install,
-          {
-            name: 'ESLint',
-            run: 'cd ${{ matrix.path }} && pnpm run lint:js',
-          },
-        ],
-      },
-    };
   }
 
   if (config.support?.typescript) {
     assert(Array.isArray(config.support.typescript), 'expected support.typescript to be an array');
-
-    let isGlint = config.support?.glint ?? false;
-
-    let buildCommand = isGlint ? `glint` : 'tsc --build';
-
-    typescript = {
-      'typescript-compatibility': {
-        name: '${{ matrix.typescript-scenario }}',
-        ...system,
-        'continue-on-error': true,
-        needs: ['build'],
-        strategy: {
-          'fail-fast': true,
-          matrix: {
-            'typescript-scenario': config.support.typescript,
-          },
-        },
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...installNoFrozen,
-          ...getDist,
-          {
-            name: 'Update TS Version',
-            run: 'pnpm add --save-dev ${{ matrix.typescript-scenario }}',
-            'working-directory': testApp,
-          },
-          {
-            name: 'Type checking',
-            run:
-              `pnpm --filter ${testAppName} exec tsc -v; ` +
-              (isGlint ? `pnpm --filter ${testAppName} exec glint --version; ` : '') +
-              `pnpm --filter ${testAppName} exec ${buildCommand}`,
-          },
-        ],
-      },
-    };
   }
 
   if (support?.['ember-try']) {
@@ -216,198 +90,77 @@ async function buildCi(config, options) {
 
     if (support['ember-try'] === true) {
       scenarios = await getEmberTryNames(config, options);
-    } else if (Array.isArray(support['ember-try'])) {
-      scenarios = support['ember-try'];
-    } else {
-      assert(
-        false,
-        'Unknown value type for support.ember-try. Expected an array of scenario names or `true`.'
-      );
     }
 
-    tryScenarios = {
-      'try-scenarios': {
-        name: '${{ matrix.ember-try-scenario }}',
-        ...system,
-        'timeout-minutes': 10,
-        needs: ['tests'],
-        strategy: {
-          'fail-fast': true,
-          matrix: {
-            'ember-try-scenario': scenarios,
-          },
-        },
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...installNoLockfile,
-          ...getDist,
-          {
-            name: 'test',
-            'working-directory': testApp,
-            run: `node_modules/.bin/ember try:one \${{ matrix.ember-try-scenario }} --skip-cleanup`,
-          },
-        ],
-      },
-    };
+    config.tryScenarios = scenarios;
   }
 
   if (release?.semantic) {
-    publish = {
-      release: {
-        name: 'Release',
-        ...system,
-        if: `github.ref == 'refs/heads/${defaultBranch}'`,
-        needs: [
-          'tests',
-          ...(Object.keys(typescript).length ? ['typescript-compatibility'] : []),
-          ...(Object.keys(tryScenarios).length ? ['try-scenarios'] : []),
-          'floating-deps-tests',
-        ],
-        steps: [
-          { uses: actions.checkout, with: { 'persist-credentials': false } },
-          { uses: actions.volta },
-          ...depCache,
-          ...pnpm,
-          ...install,
-          ...getDist,
-          {
-            name: 'Release',
-            run: './node_modules/.bin/semantic-release',
-            'working-directory': addon,
-            env: {
-              NPM_TOKEN: '${{ secrets.NPM_TOKEN }}',
-              GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
-            },
-          },
-        ],
-      },
-    };
+    /**
+     * Names of jobs in ci.yml
+     */
+    config.releaseRequirements = [
+      'default_tests',
+      'floating_tests',
+      ...(Object.keys(config.support?.typescript || {}).length ? ['typecheck'] : []),
+      ...(config.support?.['ember-try'] ? ['try_scenarios'] : []),
+    ];
   }
+
+  config.extra = [];
 
   if (extra) {
     assert(Array.isArray(extra), 'Expected extra entry to be an array');
 
     for (let extraJob of extra) {
-      let builtExtra = {
+      config.extra.push({
         ...extraJob,
-        ...system,
-        steps: [...init, ...depCache, ...pnpm, ...install, ...getDist, ...extraJob.steps],
-      };
-
-      let id =
-        builtExtra.id ||
-        (builtExtra.name ?? `extra_${extra.indexOf(extraJob)}`)
-          .split('')
-          .filter((char) => char.match(/\S/))
-          .join('');
-
-      extras[id] = builtExtra;
+        id:
+          extraJob.id ||
+          (extraJob.name ?? `extra_${extra.indexOf(extraJob)}`)
+            .split('')
+            .filter((char) => char.match(/\S/))
+            .join(''),
+        steps: extraJob.steps.map((step) => indent(4)).join('\n'),
+      });
     }
   }
 
-  let assertContents = yaml.dump({
-    target: env.dist,
-    setup: { run: build.run, cwd: addon },
-    expect: build.expect,
-  });
+  for await (const file of klaw(templateFiles)) {
+    if (file.stats.isDirectory()) continue;
 
-  let ci = {
-    name: 'CI',
-    on: {
-      pull_request: null,
-      push: {
-        branches: [defaultBranch],
-      },
-      schedule: [
-        // every Sunday at 3am
-        { cron: '0 3 * * 0 ' },
-      ],
-    },
+    let relative = path.relative(templateFiles, file.path);
+    let destination = path.join(options.cwd, '.github', relative.replace(/\.ejs$/, ''));
+    let destinationDir = path.dirname(destination);
 
-    env,
+    await fse.mkdirp(destinationDir);
 
-    jobs: {
-      install_dependencies: {
-        name: 'Install Dependencies',
-        ...system,
-        steps: [...init, ...depCache, ...pnpm, ...install],
-      },
-      ...eslint,
-      ...(config.lint?.commits
-        ? {
-            commits: {
-              name: 'Commit Messages',
-              ...system,
-              steps: [
-                {
-                  uses: actions.checkout,
-                  with: { 'fetch-depth': 0 },
-                },
-                { uses: actions.volta },
-                { uses: actions.commitlint },
-              ],
-            },
-          }
-        : {}),
-      build: {
-        name: 'Build Tests',
-        needs: ['install_dependencies'],
-        ...system,
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...install,
-          {
-            name: 'Build and Assert Output',
-            run: stripIndent`
-              echo '${assertContents}' >> assert-contents.config.yml
-              npx assert-folder-contents
-            `,
-          },
-          {
-            uses: actions.uploadArtifact,
-            with: { name: 'dist', path: '${{ env.dist }}' },
-          },
-        ],
-      },
+    console.info(`Processing ${relative}`);
 
-      tests: {
-        name: 'Default Tests',
-        ...system,
-        needs: ['build'],
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...install,
-          ...getDist,
-          { run: `pnpm --filter ${testAppName} run test:ember` },
-        ],
-      },
-      'floating-deps-tests': {
-        name: 'Floating Deps Test',
-        ...system,
-        needs: ['build'],
-        steps: [
-          ...init,
-          ...depCache,
-          ...pnpm,
-          ...installNoLockfile,
-          ...getDist,
-          { run: `pnpm --filter ${testAppName} run test:ember` },
-        ],
-      },
-      ...tryScenarios,
-      ...typescript,
-      ...publish,
-      ...extras,
-    },
-  };
+    if (file.path.endsWith('.ejs')) {
+      let templateContent = await fse.readFile(file.path);
+      let content = ejs.render(templateContent.toString(), {
+        ...config,
+        env,
+        defaultBranch,
+        libraryName,
+        testAppName,
+        utils: {
+          toArray: toYamlArray,
+          toMultiLineArray,
+          stringify,
+          indent,
+          eachLine,
+        },
+      });
 
-  return ci;
+      await fse.writeFile(destination, content);
+      continue;
+    }
+
+    // Copy to destination
+    await fse.copyFile(file.path, destination);
+  }
 }
 
 /**
